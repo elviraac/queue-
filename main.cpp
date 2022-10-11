20b43f8ec92b10f98546dc01a7df5e00d6373e42
elviraac <115537220+elviraac@users.noreply.github.com>
2022-10-11 21:26:24 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,131 +1,254 @@
 #include <iostream>
+#include <string>
+#define MAX 20
 using namespace std;
-#define MAX 10
+
+int top = -1;
+int stk [MAX];
 
 
-struct Antrian {
-    int data[MAX];
-    int head;
-    int tail;
-};
-
-void enqueue(int data);
-int dequeu();
-void tampil();
-int main()
-{
-    int pilih,indata;
-    //pilih program.
-    pilihprogram:
-    cout<<"Pilih program\n";
-
-    cout<<"1. Menambahkan data\n";
-    cout<<"2. Menghapus data\n";
-    cout<<"3. Menampilkan data\n";
-
-    cout<<"\n Masukan Pilihan : \n\n";
-    cin>>pilih;
-
-    switch(pilih){
-        case 1 : cout<<"Masukkan data : ";
-                 cin>>indata;
-                 enqueue(indata);
-                 goto pilihprogram;
-        break;
-
-        case 2 : cout<<"Hapus data : \n";
-                 dequeu();
-                 goto pilihprogram;
-        break;
-
-        case 3 : cout<<"Tampilkan data : \n";
-                 tampil();goto pilihprogram;
-        break;
-
-        default :cout<<"TIDAK ADA PILIHAN";
-        break;
+int priority(char alpha){
+    if(alpha == '+' || alpha == '-'){
+        return 1;
     }
 
+    if(alpha == '*' || alpha == '/'){
+        return 2;
+    }
 
+    if(alpha == '$'){
+        return 3;
+    }
     return 0;
 }
 
-
-Antrian antre;
-
-void create(){
-    antre.head = antre.tail = -1;
-}
-
-bool isEmpty(){
-    if(antre.tail == -1){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
 bool isFull(){
-    if(antre.tail == MAX ){ //kalau max - 1 jadi nya hanya bisa memasukan 7 bilangan.
-            return true;
+    if(top == MAX - 1 ){
+        return true;
     }else {
         return false;
     }
 }
 
-//cek apakah queue penuh, jika penuh tampilkan tulisan queue penuh. jika tidak, enqueue ke queue
+bool isEmpty(){
+    if(top == -1 ){
+        return true;
+    }else {
+        return false;
+    }
+}
 
-void enqueue(int data){
+void push(char oper){
     if(isFull()){
-        cout<<"Antrian sudah penuh\n";
-    }else{
-        if(isEmpty()){
-            //kalau kosong ketika head dan tail=0
-            antre.head = antre.tail = 0;
-            antre.data[antre.tail] = data;
-            antre.head++;
-            antre.tail++;
-        }else{
-            //kalo tidak kosong
-            antre.data[antre.tail] = data;
-            //tambahkan tail karena head kan sudah 1
-            antre.tail++;
-            cout<<data<<" dimasukan kedalam antrian.\n\n";
-        }
+        cout<<"sudah penuh";
     }
-
+    else{
+        top++;
+        stk[top] = oper;
+    }
 }
 
-//cek apakah queue kosong, jika iya tampilkan tulisan queue kosong. jika tidak, dequeue dari queue
-
-int dequeu(){
-    if(isEmpty()){
-        cout<<"Antrian kosong";
+char pop(){
+    char ch;
+    if(isEmpty()==true){
+        cout<<"stack Kosong";
     }else{
-        int e = antre.data[antre.head];
-        for(int i=0;i<antre.tail;i++){
-            antre.data[i] = antre.data[i+1];
-        }
-        antre.tail--;
-        return e;
+        ch = stk[top];
+        stk[top]=NULL;
+        top--;
+        return ch;
     }
-
-
+    return 0;
 }
 
-void tampil(){
-    cout<<"\nData antrian : \n";
-    if(isEmpty()){
-        cout<<"Antrian kosong\n";
-    }else{
-        for(int i = 0;i<MAX;i++){
-            if(antre.data[i] == NULL){
-                cout<<i+1<<". Data Kosong\n";
-            }else{
-                cout<<i+1<<". "<<antre.data[i]<<endl;
+//Konversi ke Postfix
+
+string in2postfix(string infix){
+    int i = 0;
+    string pst = "";
+
+    //selama string ada isinya
+    while(infix[i]!='\0'){
+        //cek jika ketemu alfabet
+        if(infix[i]>='a' && infix[i] <= 'z'){
+            pst.insert(pst.end(),infix[i]);
+            i++;
+        }
+        //Jika menemukan bracket maka masukan dalam stack
+        else if(infix[i]=='(' || infix[i]=='{' || infix[i]=='['){
+            push(infix[i]);
+            i++;
+        }
+        //jika bertemu kurung tutup maka pop
+        else if(infix[i]==')' || infix[i]=='}' || infix[i]==']'){
+            if(infix[i] == ')'){
+                while(stk[top]!= '('){
+                    pst.insert(pst.end(),pop());
+                }
+                pop();
+                i++;
+            }
+            if(infix[i] == '}'){
+                while(stk[top]!= '{'){
+                    pst.insert(pst.end(),pop());
+                }
+                pop();
+                i++;
+            }
+            if(infix[i] == ']'){
+                while(stk[top]!= '['){
+                    pst.insert(pst.end(),pop());
+                }
+                pop();
+                i++;
+            }
+        }
+        else{
+            if(top == -1){
+                push(infix[i]);
+                i++;
+            }
+            //jika prioritas operator kuarng dari atau sma denan operat yang distack
+
+            //pop operator diposisi top dan tamnbahkan ke pst;
+
+            else if(priority(infix[i]) <= priority(stk[top])){
+                pst.insert(pst.end(),pop());
+
+                while(priority(infix[i]) == priority(stk[top])){
+                    pst.insert(pst.end(),pop());
+                    if(top<0){
+                        break;
+                    }
+                }
+                push(infix[i]);
+                i++;
+            }
+            else if(priority(infix[i]) > priority(stk[top])){
+                push(infix[i]);
+                i++;
             }
         }
     }
-    cout<<endl<<endl;
+    while(top != -1){
+        pst.insert(pst.end(),pop());
+    }
+    cout<<"hasil konversi ke postfix: "<<pst;
+    return pst;
+}
+
+//reverse
+
+void reverseStr(string& str)
+{
+    int len = str.length();
+    int n = len-1;
+    int i = 0;
+    while(i<=n){
+        //Using the swap method to switch values at each index
+        swap(str[i],str[n]);
+        n = n-1;
+        i = i+1;
+  }
+
+}
+
+//Konversi ke prefix
+string in2prefix(string infix){
+    int i = 0;
+    reverseStr(infix);
+    string pst = "";
+
+    //selama string ada isinya
+    while(infix[i]!='\0'){
+
+        //infix dibaca dari kanan ke kiri berarti braket tutup bernilai braket terbuka.
+
+        //cek jika ketemu alfabet
+        if(infix[i]>='a' && infix[i] <= 'z'){
+            pst.insert(pst.begin(),infix[i]);
+            i++;
+        }
+        //Jika menemukan bracket maka masukan dalam stack
+        else if(infix[i]==')' || infix[i]=='}' || infix[i]==']'){
+            push(infix[i]);
+            i++;
+        }
+        //jika bertemu kurung buka maka pop
+        else if(infix[i]=='(' || infix[i]=='{' || infix[i]=='['){
+            if(infix[i] == '('){
+                while(stk[top]!= ')'){
+                    pst.insert(pst.begin(),pop());
+                }
+                pop();
+                i++;
+            }
+            if(infix[i] == '{'){
+                while(stk[top]!= '}'){
+                    pst.insert(pst.begin(),pop());
+                }
+                pop();
+                i++;
+            }
+            if(infix[i] == '['){
+                while(stk[top]!= ']'){
+                    pst.insert(pst.begin(),pop());
+                }
+                pop();
+                i++;
+            }
+        }
+        else{
+            if(top== -1){
+                push(infix[i]);
+                i++;
+            }
+            //jika prioritas operator kuarng dari atau sma denan operat yang distack,
+            //pop operator diposisi top dan tamnbahkan ke pst;
+
+            else if(priority(infix[i]) <= priority(stk[top])){
+                pst.insert(pst.begin(),pop());
+
+                while(priority(infix[i]) == priority(stk[top])){
+                    pst.insert(pst.begin(),pop());
+                    if(top<0){
+                        break;
+                    }
+                }
+                push(infix[i]);
+                i++;
+            }
+            else if(priority(infix[i]) > priority(stk[top])){
+                push(infix[i]);
+                i++;
+            }
+        }
+    }
+    while(top != -1){
+        pst.insert(pst.begin(),pop());
+    }
+    cout<<"hasil konversi ke prefixfix: "<<pst;
+    return pst;
+}
+
+
+
+
+
+int main()
+{
+    string infix,postfix,prefix,tukar;
+    string tes;
+
+    cin>>tes;
+    cout<<"Berikut postfix : \n";
+    postfix = in2postfix(tes);
+    cout<<endl;
+
+    cout<<endl;
+    cout<<"Berikut prefix : \n";
+    prefix = in2prefix(tes);
+
+
+    return 0;
 }
